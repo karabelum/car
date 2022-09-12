e29aa53809136ae236dc92701bcffb6d87c8769f
karabelum <saturnday@yandex.ru>
2022-09-12 16:27:28 +0500

    Добавьте файлы проекта.

---
+++ test/Source.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+//enum PCState {
+//	ON,OFF,SLEEP
+//};
+//class PC{
+//public:
+//	PCState GetState() { return State; }
+//	void SetState(PCState state){
+//		this->State = state;
+//	}
+//private:
+//	PCState State;
+//}
+//enum PCState {
+//	on,off,sleep
+//};
+class Car {
+public:
+	int wight{100};
+	int length;
+	std::string name;
+}my_car, not_my_car;
+class bus {
+	Car door;
+};
+int main() {
+	my_car.length;
+	not_my_car.name;
+
+	return 0;
+}
